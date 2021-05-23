fe518766a04e958292592c53a308ce3f97db22e9
KavinduUoM20 <kavindums834@gmail.com>
2021-05-23 12:13:32 +0530

    POS-CustomerCRUD_Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+/* tslint:disable:whitespace */
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CustomerPageComponent} from './Components/Common/customer-page/customer-page.component';
+import {ItemPageComponent} from './Components/Common/item-page/item-page.component';
+import {OrderDetailsPageComponent} from './Components/Common/order-details-page/order-details-page.component';
+import {OrderPageComponent} from './Components/Common/order-page/order-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'customer',component:CustomerPageComponent},
+  {path:'item',component:ItemPageComponent},
+  {path:'order',component: OrderPageComponent},
+  {path:'order-details',component:OrderDetailsPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/Share/header/header.component';
+import { CustomerPageComponent } from './Components/Common/customer-page/customer-page.component';
+import { ItemPageComponent } from './Components/Common/item-page/item-page.component';
+import { OrderPageComponent } from './Components/Common/order-page/order-page.component';
+import { OrderDetailsPageComponent } from './Components/Common/order-details-page/order-details-page.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material/tabs';
+import { AlertUIComponent } from './Components/Share/alert-ui/alert-ui.component';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CustomerPageComponent,
+    ItemPageComponent,
+    OrderPageComponent,
+    OrderDetailsPageComponent,
+    AlertUIComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatTabsModule,
+        MatDialogModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
